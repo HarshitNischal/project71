176f56542fcded26b49980c753b89b45e77f5786
HarshitNischal <71174516+HarshitNischal@users.noreply.github.com>
2021-07-04 18:51:27 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,57 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { createAppContainer } from "react-navigation";
-import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import TransactionScreen from "./screens/BookTransactionScreen";
-import SearchScreen from "./screens/SearchScreen";
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer,createSwitchNavigator} from "react-navigation" ;
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import SearchScreen from "./screen/searchScreen";
+import TransactionScreen from "./screen/transactionScreen";
+import LoginScreen from "./screen/loginScreen";
+import loginScreen from './screen/loginScreen';
 
-export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+
+export default function App() {
+  return (
+   <AppContainer/>
+  );
 }
-
-const TabNavigator = createBottomTabNavigator({
-  Transaction: { screen: TransactionScreen },
-  Search: { screen: SearchScreen }
-
+var TabNavigator=createBottomTabNavigator({
+Search:{screen:SearchScreen},
+Transaction:{screen:TransactionScreen}
 },
-{
-  defaultNavigationOptions:({navigation})=>({
-    tabBarIcon: ({})=>{
-      const routeName=navigation.state.routeName;
-      if(routeName=== 'Transaction'){
-        return(
-          <Image source={
-            require('../assets/book.png') }
-            style={{width:40, height:40}} 
-          />
-          
-        )
-      }
-      else if(routeName=== 'Search'){
-        return(
-          <Image source={
-            require('../assets/searchingbook.png') }
-            style={{width:40, height:40}}
+{defaultNavigationOptions:({navigation})=>{
+  tabBarIcon:()=>{
+    const routeName=navigation.state.routeName
+    if(routeName==='Transaction'){
+      return(
+        <Image
+        source={require('./assets/book.png')}
+        style={{width:20,
+        height:20}}
         />
-        )
-      }
+      )
     }
-  })
-}
-);
-
-
-
-const AppContainer = createAppContainer(TabNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center"
+    else if(routeName==='Search'){
+      return(
+        <Image
+        source={require('./assets/searchingbook.png')}
+        style={{width:100,
+          height:100}}
+        />
+      )
+    }
   }
-});
+}
+}
+)
+const SwitchNavigator=createSwitchNavigator({
+  LoginScreen:{screen:loginScreen},
+  TabNavigator:{screen:TabNavigator}
+})
+const AppContainer=createAppContainer(SwitchNavigator)
